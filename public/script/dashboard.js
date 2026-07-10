601c902221e79bb1fcd8f1a6fdf34e2e0ce2f329
ASHOK <lakkideveloper@gmail.com>
2026-07-10 19:28:41 +0530

    API integration

--- public/script/dashboard.js
+++ public/script/dashboard.js
@@ -2,6 +2,6 @@ $(function() {
     const userData = localStorage.getItem("user");
     const user = JSON.parse(userData);
     console.log("Name:", user);
-    $("#welcomeMessage").text(`Welcome ${user.name}!`);
+    $("#welcomeMessage").text(`Welcome ${user.Name}!`);
 
 });
